49844ebf67c5cfd240ab31ee9ed21a585fbad571
Abián <abian.camejo@gmail.com>
2024-12-02 09:57:24 +0000

    probando git

---
+++ probandoEntity/DB/BookContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DB
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options) 
+        { 
+        
+        }
+        public DbSet<User> Users { get; set; } 
+    }
+}
---
+++ probandoEntity/probandoEntity/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using DB;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace probandoEntity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private BookContext _context;
+
+        public UserController(BookContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IEnumerable<User> Get() => _context.Users.ToList();
+    }
+}
